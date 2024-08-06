34d9e7c1becb58267be871475370f8304317dd88
qywi <teslagamnesla.123456789@gmail.com>
2024-08-06 19:13:36 +0300

    feat: now it works correctly

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   const stack = [];
 
-  for (const c of str) {
-      if (bracketsConfig.some(pair => pair[0] === c)) {
-          stack.push(c);
-      } else if (bracketsConfig.some(pair => pair[1] === c)) {
-          if (stack.length === 0) {
-              return false;
-          }
-          const ver = stack.pop();
-          if (!bracketsConfig.some(pair => pair[0] === ver && pair[1] === c)) {
-              return false;
-          }
+  const bracketMap = new Map(bracketsConfig);
+
+  for (let i = 0; i < str.length; i++) {
+    let symbol = str[i];
+
+    if (bracketMap.has(symbol)) {
+      if (stack.length > 0 && stack[stack.length - 1] === symbol && bracketMap.get(symbol) === symbol) {
+        stack.pop();
+      } else {
+        stack.push(symbol);
       }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (bracketMap.get(topElement) === symbol) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
   }
 
-  return stack.length === 0 ? true : false;
-}
+  return stack.length === 0;
+};
